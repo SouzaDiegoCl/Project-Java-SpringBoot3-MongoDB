c94b42a0811f2b2cdcdc3e5281342680d02e4aa5
SouzaDiegoCl <souzadiegocl@gmail.com>
2024-06-28 01:57:30 -0300

    Operação DTO para get users

--- src/main/java/com/diego/workshopmongo/resources/UserResource.java
+++ src/main/java/com/diego/workshopmongo/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.diego.workshopmongo.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.diego.workshopmongo.domain.User;
+import com.diego.workshopmongo.dto.UserDTO;
 import com.diego.workshopmongo.services.UserService;
 
 @Controller
@@ -23,9 +25,10 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){		
+	public ResponseEntity<List<UserDTO>> findAll(){		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
