e5f05c52af90b087b723eb58ee44f03749cc35af
SouzaDiegoCl <souzadiegocl@gmail.com>
2024-06-30 22:02:25 -0300

    Correcao nomenclatura instantiation

--- src/main/java/com/diego/workshopmongo/config/Instantiation.java
+++ src/main/java/com/diego/workshopmongo/config/Instantiation.java
@@ -10,7 +10,7 @@ import com.diego.workshopmongo.domain.User;
 import com.diego.workshopmongo.repository.UserRepository;
 
 @Configuration
-public class Instatiation implements CommandLineRunner {
+public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
