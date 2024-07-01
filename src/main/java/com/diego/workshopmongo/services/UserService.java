ae169e41eab2f767e91578a1637fba56abb81252
SouzaDiegoCl <souzadiegocl@gmail.com>
2024-06-30 22:02:02 -0300

    Obtendo user pelo Id

--- src/main/java/com/diego/workshopmongo/services/UserService.java
+++ src/main/java/com/diego/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.diego.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diego.workshopmongo.domain.User;
 import com.diego.workshopmongo.repository.UserRepository;
+import com.diego.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,9 @@ public class UserService {
 		return repository.findAll();		
 	}
 	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
 }
