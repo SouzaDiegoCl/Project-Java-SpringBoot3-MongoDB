0411b47b630865a1e1babdbcfb4f96cea569e242
SouzaDiegoCl <souzadiegocl@gmail.com>
2024-07-07 16:41:58 -0300

    Consulta simples com @query

--- src/main/java/com/diego/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/diego/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.diego.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.diego.workshopmongo.domain.Post;
@@ -10,6 +11,10 @@ import com.diego.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	//?0 recebe o primeiro parametro que vier no metodo, no caso o text
+	@Query("{ 'title': { $regex: ?0 , $options: 'i' } }")
+	List<Post> 	 searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
